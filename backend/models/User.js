6e75dc0af16282cd47b44fdcabc796b43842122b
aninex97 <aninex97@gmail.com>
2025-10-17 17:30:42 +0545

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -34,4 +34,5 @@ userSchema.methods.correctPassword = async function(candidatePassword, userPassw
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+export default User;
